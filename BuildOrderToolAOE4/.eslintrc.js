9cf72f6ecac513b0d85ece1b2124f27c03dba935
Caleb Fetzer <caleb.fetzer@gmail.com>
2022-03-13 11:08:13 +0800

    Initial commit with re-arrange, eslint, prettier into VITE

---
+++ BuildOrderToolAOE4/.eslintrc.js
@@ -0,0 +1,13 @@
+/* eslint-disable no-undef */
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["eslint:recommended", "plugin:prettier/recommended"],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  rules: {},
+};
